e6c7df8cb2d5ac3a45e73f7cb90aa8443ef068d1
jerryseigle <mr.jerryseigle@gmail.com>
2022-12-01 02:37:53 -0500

    added typescript definition
    I added added typescript definition for users that would like to use this library with typescript. 
    Each function has a  param type of any. This is to ensure that the user can pass any param and check if its valid or not

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export declare function isIPAddress(ipAddress:any): true | false
+export declare function isIPV4Address(ipV4Address:any): true | false
+export declare function isIPV6Address(ipV6Address:any): true | false
+export declare function ipVersion(ipAddress:any): string
